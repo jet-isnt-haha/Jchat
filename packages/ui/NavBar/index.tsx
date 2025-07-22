5fedc27a4e6e7291e0d8f2141ed9269faff40e5f
jet-isnt-haha <1802966308@qq.com>
2025-07-22 21:10:51 +0800

    feat(typescript): home页面布局基础

---
+++ packages/ui/NavBar/index.tsx
@@ -0,0 +1,33 @@
+import {
+	NavBar as AntdNavBar,
+	type NavBarProps as AntdNavBarProps
+} from 'antd-mobile';
+import styles from './index.module.less';
+import type { CSSProperties } from 'react';
+import { clsx } from 'clsx';
+
+interface NavBarProps extends Omit<AntdNavBarProps, 'onBack'> {
+	children?: React.ReactNode;
+	className?: string;
+	style?: CSSProperties;
+	onBack?: () => void;
+}
+
+const SideBarIcon = <div style={{ fontSize: '0.5rem' }}>SideBarIcon</div>;
+const NewChatIcon = <div style={{ fontSize: '0.5rem' }}>NewChatIcon</div>;
+
+const NavBar = ({ children = 'JetBot', className, style }: NavBarProps) => {
+	return (
+		<AntdNavBar
+			backIcon={false}
+			className={clsx(styles.nav, ' - ', className)}
+			left={SideBarIcon}
+			right={NewChatIcon}
+			style={style}
+		>
+			{children}
+		</AntdNavBar>
+	);
+};
+
+export default NavBar;
--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,18 @@ import { fileURLToPath } from 'node:url';
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	css: {
+		preprocessorOptions: {
+			less: {
+				// 可选：全局 less 变量、mixin 也可以配置在这里
+				javascriptEnabled: true
+			}
+		},
+		modules: {
+			//让 *.module.less 生效
+			generateScopedName: '[name]__[local]__[hash:base64:5]'
+		}
+	},
 	resolve: {
 		alias: {
 			'@': fileURLToPath(new URL('./src', import.meta.url)),
